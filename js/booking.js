44c8adac8157255b2e724e93693c477c363d085f
Dmitrii Kachuk <maiiamphiphi@gmail.com>
2025-12-24 12:16:14 +0700

    fix any bag

--- js/booking.js
+++ js/booking.js
@@ -3,17 +3,25 @@ const navMenu = document.querySelector('.header__nav');
 const navLink = document.querySelectorAll('.header__nav-link');
 const overlay = document.querySelector('.overlay');
 
+// ===== FUNCTION FOR OPEN NAVIGATION MENU =====
+
 function openNav() {
 	navMenu.classList.add('nav__open');
 	openMenuButton.classList.add('nav__open');
 	overlay.classList.add('nav__open');
 }
 
+// ===== FUNCTION FOR CLOSE NAVIGATION MENU =====
+
 function closeNav() {
 	navMenu.classList.remove('nav__open');
 	openMenuButton.classList.remove('nav__open');
 	overlay.classList.remove('nav__open');
 }
+
+// ===== EVENT LISTENERS =====
+
+// ===== OPEN NAVIGATION MENU BY CLICKING ON BURGER BUTTON =====
 
 openMenuButton.addEventListener('click', () => {
 	if (navMenu.classList.contains('nav__open')) {
@@ -23,11 +31,15 @@ openMenuButton.addEventListener('click', () => {
 	}
 });
 
+// ===== CLOSE NAVIGATION MENU BY CLICKING ON LINK =====
+
 navLink.forEach((link) => {
 	link.addEventListener('click', () => {
 		closeNav();
 	});
 });
+
+// ===== CLOSE NAVIGATION MENU BY CLICKING ON OVERLAY =====
 
 overlay.addEventListener('click', function(event) {
 	let target = event.target;
